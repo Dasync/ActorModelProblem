ba979ead7b6fb444190e42870b09f805e1e6f665
Serge Semenov <sergiis@live.com>
2018-11-10 11:39:31 -0800

    OrderPlacementCoordinator draft

---
+++ OrderPlacementCoordinator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class PurchaseItemCommand : Message
+{
+    public string ItemId { get; set; }
+    public int Quantity { get; set; }
+}
+
+public class OrderPlacementCoordinator : Actor,
+    IReceive<PurchaseItemCommand>
+{
+    private PersistedValue<Guid> transactionId;
+
+    public void Receive(PurchaseItemCommand command)
+    {
+        this.transactionId = PersistedValue.Create(Guid.NewGuid());
+    }
+}
